a12678b3b39605b4ee86fc1102d2d88b1891835d
Chutchanan Maneerueng <134831110+Chutchanan@users.noreply.github.com>
2024-01-11 22:52:30 +0700

    Update common_functions.py

--- includes/common_functions.py
+++ includes/common_functions.py
@@ -5,8 +5,17 @@
 # COMMAND ----------
 
 def merge_delta_data(input_df, db_name, table_name, folder_path, merge_condition, partition_column):
+'''
+Merge (Update/Insert) data
+Input Parameter
+1. input_df = The new data that we want to insert to the existing table
+2. db_name = database name
+3. table_name = table_name
+4. folder_path = folder_path
+5. merge_condition = condition(s) that we use to merge the table
+6. partition_column = column that we want to partition.
+'''
   spark.conf.set("spark.databricks.optimizer.dynamicPartitionPruning","true")
-
   from delta.tables import DeltaTable
   if (spark._jsparkSession.catalog().tableExists(f"{db_name}.{table_name}")):
     deltaTable = DeltaTable.forPath(spark, f"{folder_path}/{table_name}")
